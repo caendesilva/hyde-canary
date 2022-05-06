dacdad0cf6ac327c8c665f91806fbd7414246edd
Caen De Silva <caen@desilva.se>
2022-05-06 16:14:06 +0200

    Format HTML

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -1,7 +1,7 @@
 @extends('hyde::layouts.app')
 @section('content')
- @push('styles')
- <style>
+@push('styles')
+<style>
     /* Gradients by https://uigradients.com/ */
     #app {
         /* Royal */
@@ -19,27 +19,26 @@
         display: none;
     }
 </style>
- @endpush
-    <main class="my-auto px-6 pb-12 antialiased app-gradient-dark">
-        <div class="mx-auto max-w-7xl">
-            <!-- Main Hero Content -->
-            <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
-                <h1 class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-100 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
-                    <span class="block text-4xl md:text-5xl mb-4 sm:mb-0">You're running on </span><span
-                        class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br logo-gradient md:inline-block drop-shadow-2xl tracking-normal">HydePHP</span>
-                    <small><i>CANARY</i></small>
-                </h1>
-                <div class="mx-auto mt-8 sm:mt-4 text-gray-200 md:mt-8 md:max-w-2xl md:text-center">
-                    <section aria-label="About Hyde">
-                        <p class="lg:text-lg">
-                            Leap into the future of static HTML blogs and documentation with the tools you already know and love.
-                            Made with Tailwind, Laravel, and Coffee.
-                        </p>
-                    </section>
-                </div>
+@endpush
+<main class="my-auto px-6 pb-12 antialiased app-gradient-dark">
+    <div class="mx-auto max-w-7xl">
+        <!-- Main Hero Content -->
+        <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
+            <h1 class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-100 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
+                <span class="block text-4xl md:text-5xl mb-4 sm:mb-0">You're running on </span><span
+                    class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br logo-gradient md:inline-block drop-shadow-2xl tracking-normal">HydePHP</span>
+                <small><i>CANARY</i></small>
+            </h1>
+            <div class="mx-auto mt-8 sm:mt-4 text-gray-200 md:mt-8 md:max-w-2xl md:text-center">
+                <section aria-label="About Hyde">
+                    <p class="lg:text-lg">
+                        Leap into the future of static HTML blogs and documentation with the tools you already know and love.
+                        Made with Tailwind, Laravel, and Coffee.
+                    </p>
+                </section>
             </div>
-            <!-- End Main Hero Content -->
         </div>
-    </main>
-
+        <!-- End Main Hero Content -->
+    </div>
+</main>
 @endsection
